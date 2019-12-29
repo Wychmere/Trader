7c668936ccf308a12f5be2cdf56a9b362212f175
ggiv <ggeorgiev.xxx@gmail.com>
2019-12-29 23:22:18 +0200

    Added stop orders to the strategy file.

--- strategy.py
+++ strategy.py
@@ -10,11 +10,15 @@ first_order_type = 'limit'
 # For Market orders set all prices to None.
 # For Limit orders set stop prices to None.
 # For Stop Limit orders set both limit and stop prices to some value.
+# For Stop orders set stop prices to some value.
 buy_limit_price = 199
 buy_stop_price = None
 
 sell_limit_price = 220
 sell_stop_price = None
+
+buy_stop_price = 199
+sell_stop_price = 201
 
 # Valid values for time_in_force: day, gtc, opg, cls, ioc, fok
 time_in_force = 'gtc'
