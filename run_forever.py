481c1ba1fbcbd895565efe2281f00bbc86559083
ggiv <ggeorgiev.xxx@gmail.com>
2020-06-30 15:22:23 +0300

    Added error handling to run_forever.

--- run_forever.py
+++ run_forever.py
@@ -1,11 +1,22 @@
 import sys
+import time
+import traceback
 from subprocess import Popen
 
 python_command = sys.argv[1]
 filename = sys.argv[2]
 
+sleep_after_error = 10
+
+command = '{} {}'.format(python_command, filename)
+
 while True:
-    command = '{} {}'.format(python_command, filename)
-    print(f'Starting {filename}')
-    p = Popen(command, shell=True)
-    p.wait()
+    try:
+        print(f'Starting {filename}')
+        p = Popen(command, shell=True)
+        p.wait()
+    except:
+        err = traceback.format_exc()
+        print(f'{filename} failed:\n{err}')
+        print(f'Restarting {filename} in {sleep_after_error} seconds.')
+        time.sleep(sleep_after_error)
